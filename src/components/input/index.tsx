f2b4a28a65882ef8e3b581c138eaaef96664b074
Nelson <punkpunkada@gmail.com>
2026-07-14 14:14:28 -0300

    login component and screen

---
+++ src/components/input/index.tsx
@@ -0,0 +1,26 @@
+import type { RegisterOptions, UseFormRegister } from 'react-hook-form';
+interface InputProps {
+   placeholder: string;
+   type: string;
+   name: string;
+   register?: UseFormRegister<any>;
+   errors?: string;
+   rules?: RegisterOptions;
+}
+
+export function Input({ placeholder, type, name, register, errors, rules }: InputProps) {
+    return (
+        <div>
+            <input
+                className="w-full border-2 rounded-md h-11 px-2"
+                type={type}
+                placeholder={placeholder}
+                name={name}
+                {...(register ? register(name, rules) : {})}
+                id={name}
+             />
+            {errors && <span className="text-red-500 text-sm">{errors}</span>}
+        </div>
+    )
+
+}
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,59 @@
+import { Link } from 'react-router-dom';
+import logoImg from '../../assets/logo.svg';
+import { Container } from '../../components/container';
+import { Input } from '../../components/input';
+import { useForm} from "react-hook-form";
+import {z} from "zod";
+import { zodResolver } from '@hookform/resolvers/zod/src/index.js';
+
+const schema = z.object({
+  email: z.string().email("Digite um email válido").nonempty("O email é obrigatório"),
+  password: z.string().min(6, "A senha deve ter pelo menos 6 caracteres").nonempty("A senha é obrigatória"),
+})
+
+type FormData = z.infer<typeof schema>
 
 export function Login() {
+  const { register, handleSubmit, formState:{errors} } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    mode: "onChange",
+  });
+
+  function onSubmit(data: FormData) {
+    console.log(data);
+  }
+
   return (
-    <div>
-      <h1>Welcome to the Login Page</h1>
-      <p>Please enter your credentials to log in.</p>
-    </div>
+    <Container>
+      <div className="w-full min-h-screen flex justify-center items-center flex-col gap-4">
+        <Link to="/" className="mb-6 max-w-sm w-full">
+          <img src={logoImg} alt="Logo" className="w-full" />
+        </Link>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="bg-white max-w-xl w-full rou nded-lg">
+          <div className="mb-3">
+            <Input
+            type="text"
+            placeholder="Digite seu email"
+            name="email"
+            errors={errors.email?.message}
+            register={register}
+          />
+
+          </div>
+          <div className="mb-3">
+            <Input
+            type="password"
+            placeholder="Digite sua senha"
+            name="password"
+            errors={errors.password?.message}
+            register={register}
+          />
+
+          </div>
+          <button type="submit">Acessar</button>
+        </form>
+      </div>
+    </Container>
   );
 }
