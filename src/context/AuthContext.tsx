daad3b16024ffb0a90670248837883c8f70876b8
Nelson <punkpunkada@gmail.com>
2026-07-16 14:42:52 -0300

    context api to know if user are loged

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,55 @@
+import { createContext, useState, useEffect } from "react";
+import type { ReactNode } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../services/firebaseConnection";
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+interface UserProps {
+    uid: string;
+    name: string | null;
+    email: string | null;
+}
+
+type AuthContextData = {
+    signed: boolean;
+    loadingAuth: boolean;
+}
+
+export const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({children}: AuthProviderProps) {
+    
+    const [user , setUser] = useState<UserProps | null>(null);
+    const [loadingAuth, setLoadingAuth] = useState(true);
+    
+    useEffect(()=>{
+        const unsubscribe = onAuthStateChanged(auth, (user)=>{
+            if(user){
+                setUser({
+                    uid: user.uid,
+                    name: user?.displayName,
+                    email: user?.email
+                });
+                setLoadingAuth(false);
+            }else{
+                setUser(null);
+                setLoadingAuth(false);
+            }
+        });
+
+        return () => unsubscribe();
+    },[]);
+
+
+
+    return(
+        <AuthContext.Provider value={{signed: !!user, loadingAuth: loadingAuth}}> 
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthProvider;
